180d7a0660daf8d754d8f2349f77e8f546c9cf82
Rayne <time.lord.iren@gmail.com>
2021-04-13 00:35:22 +0200

    player controller added

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,23 +4,63 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    CharacterController controller;
-    float speed = 2;
+    private CharacterController controller;
+    private float speed = 5f;
+    private float gravity = -9.81f;
+    private float groundDistance = 0.9f;
+    [SerializeField] private LayerMask groundMask;
+    private Vector3 velocity;
 
-    // Start is called before the first frame update
+    bool isGrounded;
+
+    [SerializeField] private Transform groundCheck;
+
+    private float mouseSensitivity = 140f;
+    private float rotationOnX;
+    [SerializeField] private Transform mainCamera;
+
+
     void Start()
     {
         controller = gameObject.GetComponent<CharacterController>();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Movement
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        // Rotation
+
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * mouseSensitivity;
+        float m_X = Input.GetAxis("Mouse X") * Time.deltaTime * mouseSensitivity;
+
+        rotationOnX -= mouseY;
+        rotationOnX = Mathf.Clamp(rotationOnX, -90f, 90f);
+        mainCamera.localEulerAngles = new Vector3(rotationOnX, 0f, 0f);
+
+        transform.Rotate(Vector3.up * m_X);
     }
+
+    private void 
 }
